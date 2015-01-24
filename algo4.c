773506944eb880fc414a28f55bd6117e3f10d3a8
kathleen beltramini <beltramk@onid.oregonstate.edu>
2015-01-23 23:28:22 -0800

    Update algo4.c

--- algo4.c
+++ algo4.c
@@ -26,21 +26,47 @@ void algo4(int *test_array, int sizeofarray)
 	int endpoint, endpoint2;
 	int sum;
 	int i;
+	int count=0;
 	for (i=0; i< sizeofarray; i++)
 	{
 		max_ending_here = max_ending_here + test_array[i];
 		if(max_ending_here<0)
 		{	max_ending_here = 0;
+			count = 0;
 		}
+		else count++;
 		if (current_max < max_ending_here)
 		{	current_max = max_ending_here;
-			endpoint = i;	
-			endpoint2 = i;
-		}		
+			endpoint = endpoint2 = i;	
+			//endpoint2 = i;
+		}
 	}
+		if(endpoint<i)
+		{
+			count=count-(i-endpoint);
+		}	
+
+	//Print functionality for max sum and max array
 	printf("The maximum sum is: %d\n", current_max);
-	printf("The maximum subarray is: ");
+
+	printf("The maximum subarray is:\n");
+				
+	//tried to come up with a way to handle counting, will need to verify that it works
+	printf("Count Method:\n");
+	while(count>=0)
+	{
+		printf("%d ", test_array[endpoint-count]);	
+		count--;
+	}
+	printf("\n");
+
+
+	printf("Iterating backwards summing elements:\n");
 	sum=test_array[endpoint];
+
+	//calculates from max sum back to get the elements in the array
+	//could have also created a separate array to track the subarray
+	//so did not have to iterate backwards 
 
 	while(sum<current_max)
 	{
@@ -48,12 +74,14 @@ void algo4(int *test_array, int sizeofarray)
 		sum = test_array[endpoint]+sum;
 	}
 
+	//once reaches the start of the subarray, moves forward to
+	//display the subarray in order
 	while(endpoint2>=endpoint)
 	{
 		printf("%d ",test_array[endpoint]);
 		endpoint++;
 	}
 	printf("\n");
-	
+
 }		
 
