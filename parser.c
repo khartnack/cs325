cdddedde9cf2e30b7be740e89e8f379138cf39f9
kathleen beltramini <beltramk@onid.oregonstate.edu>
2015-02-08 11:36:30 -0800

    Update parser.c

--- parser.c
+++ parser.c
@@ -21,47 +21,59 @@ CS325 - W2015
 #include <unistd.h>
 
 //reference: http://cboard.cprogramming.com/c-programming/63552-read-numbers-file.html
-//http://stackoverflow.com/questions/10709804/read-comma-separated-numbers-from-a-file-in-c
-
-#include <stdio.h>
-#include <stdlib.h>
-
 
 int main(int argc, char **argv)
 {
-    FILE *fp;
-    int data,row,col,c,count,inc;
-    int *array, capacity=10;
-    char ch;
-    array=(int*)malloc(sizeof(int)*capacity);
-    char *filename = argv[1];
-    fp=fopen(filename,"r");
-    row=col=c=count=0;
-    while(EOF!=(inc=fscanf(fp,"%d%c", &data, &ch)) && inc == 2){
-        ++c;//COLUMN count
-        if(capacity==count)
-            array=(int*)realloc(array, sizeof(int)*(capacity*=2));
-        array[count++] = data;
-        if(ch == '\n'){
-            ++row;
-            if(col == 0){
-                col = c;
-            }
-            c = 0;
-        }
-    }
-    {   //check print
-        int i,j;
-        for(i=0;i<row;++i){
-            for(j=0;j<col;++j)
-                printf("%d ", array[i*col + j]);//matrix[i][j]
-            printf("\n");
-        }
-    }
+        char c;
+	char ch;
+	int data;
+	int inc;
+        FILE *fp;
+	char *filename = argv[1];
+	char array[10000];
+	int count = 0;
 
-//	exit;
-//exit:
-    fclose(fp);
-    free(array);
-    return 0;
+        if((fp=fopen(filename,"r"))==NULL)
+        {
+                printf("cannot open the file");
+                exit(1);
+	}
+    	else
+
+	{
+   		while(c!=EOF)
+    		{
+			int i = 0;
+       			c=fgetc(fp);
+			if (c=='[')
+			{
+				//c=fgetc(fp);
+				//printf("%c, ",c);  
+				while(ch!=']')
+				{
+					inc=fscanf(fp,"%d%c", &data, &ch);
+				        array[count++] = data;			
+				
+			}
+			}   
+			if (c=='\n')   
+			{
+				c=fgetc(fp);
+				printf("change: %c",c);
+				while(c >= '0' && c <= '9')
+				{
+					c=fgetc(fp);
+					printf("%c\n",c);
+				}
+				i=0;
+			}
+
+		}
+	while(count>0)
+	{
+		printf("%d", array[count-1]);
+		count--;
+	}
+	}
+
 }
