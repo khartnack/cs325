c7aa25aefc47df677d23b632fd28e2566e3985b7
kathleen beltramini <beltramk@onid.oregonstate.edu>
2015-02-08 12:45:54 -0800

    Update changegreedy.c

--- changegreedy.c
+++ changegreedy.c
@@ -6,42 +6,61 @@ Project Group 6
 CS325 - W2015
 */
 
-#include <sys/types.h>
-#include <dirent.h>
+
 #include <stdio.h> 
-#include <getopt.h> 
-#include <sys/utsname.h> 
-#include <time.h> 
-#include <sys/stat.h> 
-#include <string.h>
-#include <sys/types.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <unistd.h>
+
 
 //reference: http://cboard.cprogramming.com/c-programming/63552-read-numbers-file.html
 
 int main(int argc, char **argv)
 {
         char c;
+	char ch;
+	int data;
+	int change;
         FILE *fp;
 	char *filename = argv[1];
-         
+	char array[1000];
+	char coin_array[1];
+	int count = 0;
+	int chamt;
+	int array_size;
+	int i = 0;
+	int run_total;
+
         if((fp=fopen(filename,"r"))==NULL)
         {
                 printf("cannot open the file");
                 exit(1);
 	}
-    	
- 	else
+    	else
 	{
-   		while(c!=EOF)
+		while(EOF!=(c=fgetc(fp)))
     		{
-       			c=fgetc(fp);
-			if((c >= '0' && c <= '9') ||  (c == ',') || (c == '[') || (c == ']') || (c == '\n'))
-	       			 printf("%c",c);    		}   
+			if (c=='[')
+			{
+				while(ch!=']')
+				{
+					fscanf(fp,"%d%c", &data, &ch);
+				        array[count++] = data;							
+				}	
+				array_size = count;
+				count = 0;	
+ 			}
+			else
+			{
+				fscanf(fp,"%d%c", &change, &ch);
+				coin_array[0] = change;
+			}
+			chamt = coin_array[0];
+			for(i=(array_size-1); i>=0; i--)
+			{	
+				if(array[i]<chamt)
+					printf("lessthan!");	
+			}
 		}
+	}
 
-//	close(filename);
 }
+
