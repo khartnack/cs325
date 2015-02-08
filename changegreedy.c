f32a8a75ae341d194534b4405c32b9a6c45eb743
kathleen beltramini <beltramk@onid.oregonstate.edu>
2015-02-08 15:31:27 -0800

    Update changegreedy.c

--- changegreedy.c
+++ changegreedy.c
@@ -19,14 +19,12 @@ int main(int argc, char **argv)
 	FILE *fp;
 	FILE *outfp;
 	char *filename = argv[1];
-//	char *outfilename;
 	char outfilename[255];
 	int len = strlen(filename);
 	int m;
 	for(m=0; m<(len-4); m++)
 	{
 		outfilename[m]=filename[m];
-//		printf("%c", filename[m]);
 	}
 
 	outfilename[m++]='c';
@@ -41,20 +39,9 @@ int main(int argc, char **argv)
 	outfilename[m++]='t';
 	while(m<=255)
 	{
-		outfilename[m]=NULL;
+		outfilename[m]=0;
 		m++;
 	}
-//	char string;
-//	sprintf(string,outfilename);
-//	printf("%c",*outfilename);
-//	char *fn = malloc(strlen(filename+6));
-//	sprintf(fn, "%schange", filename); 
-
-//	char outfilename = &filename;
-//	char changetext[] = "change";
-//	outfilename = strcat(outfilename,changetext);
-//	printf("%c", fn);
-
 
 	char array[1000];
 	char coin_array[1];
@@ -66,7 +53,6 @@ int main(int argc, char **argv)
 	char array2[1000];
 
 	if((outfp=fopen(outfilename,"w"))==NULL)
-//	if((outfp=fopen("tester.txt","w"))==NULL)
 	{
 			printf("cannot open the file");
 			exit(1);
