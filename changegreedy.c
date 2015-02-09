856b0f76cdc84fe9d49e17528b79547bf8e0781e
kathleen beltramini <beltramk@onid.oregonstate.edu>
2015-02-09 12:02:29 -0800

    Update changegreedy.c

--- changegreedy.c
+++ changegreedy.c
@@ -8,10 +8,46 @@ CS325 - W2015
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> /* memset */
+#include <unistd.h>
+#include <assert.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <dirent.h>
+#include <math.h>     
+#include <limits.h>
 //reference: http://cboard.cprogramming.com/c-programming/63552-read-numbers-file.html
+
+void timeval_subtract (result, x, y)
+     struct timeval *result, *x, *y;
+{
+  /* Perform the carry for the later subtraction by updating y. */
+  if (x->tv_usec < y->tv_usec) {
+    int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
+    y->tv_usec -= 1000000 * nsec;
+    y->tv_sec += nsec;
+  }
+  if (x->tv_usec - y->tv_usec > 1000000) {
+    int nsec = (x->tv_usec - y->tv_usec) / 1000000;
+    y->tv_usec += 1000000 * nsec;
+    y->tv_sec -= nsec;
+  }
+
+  /* Compute the time remaining to wait.
+     tv_usec is certainly positive. */
+  result->tv_sec = x->tv_sec - y->tv_sec;
+  result->tv_usec = x->tv_usec - y->tv_usec;
+}
+
 
 int main(int argc, char **argv)
 {
+	struct timeval start, end, result;
 	char c;
 	char ch;
 	int data;
@@ -87,11 +123,10 @@ int main(int argc, char **argv)
 
 
 //Greedy Algorithm starts here 
-
+			gettimeofday(&start, NULL);
 			
-			int coin_count;
-			for(i=(array_size-1); i>=0; i--)
-			{
+			
+			int coin_count; for(i=(array_size-1); i>=0; i--) {
 				coin_count=0;
 				array2[i]=0;
 				while((array[i]<=chamt)&&(chamt>0)&& array_size>0)
@@ -104,6 +139,9 @@ int main(int argc, char **argv)
 			run_total = run_total + coin_count;
 			}
 
+			gettimeofday(&end, NULL);
+			timeval_subtract(&result, &end, &start);
+			printf("%d, %ld.%06ld\n",change, result.tv_sec, result.tv_usec);
 
 //Prints to file the array with totals per coin and the minimum # of coins
 //The coins coints are stored in array2
